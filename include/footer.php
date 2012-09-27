26b3d868a24e174f819b6c1763d2b987575a8b8f
devin148 <devinhurd@gmail.com>
2012-09-27 11:27:57 -0400

    Initial commit

---
+++ include/footer.php
@@ -0,0 +1,5 @@
+<div class="dev">
+	<p style="text-align: right;">Site design by <a href="http://www.dhurd.com">Devin Hurd</a>,
+    Pepi Simeonov,
+    and Gregory Mitrano.</p>
+</div>
---
+++ include/header.php
@@ -0,0 +1,21 @@
+<a href="index.php">
+<img class="center" style="margin-top: 10px;" src="images/banner.png" alt="ZOMGClan ZOMG Clan main site banner"
+     name="ZOMGClan Banner" />
+</a>
+
+<div class="navBar">
+    <ul>
+        <li><a href="index.php" name="ZOMG Clan home page">run back home</a></li>
+        <li><a href="blog.php" name="ZOMG Clan blog tl;dr">tl;dr the blog</a></li>
+        <li><a href="social.php" name="ZOMG Clan social networking sites">ignore us in your newsfeed</a>
+            <ul>
+                <li><a href="http://www.facebook.com/ZOMGClan">stalk us with facebook</a></li>
+                <li><a href="http://twitter.com/#!/ZomgClan">spam us tweets on twitter</a></li>
+                <li><a href="http://www.youtube.com/user/ZomgClanDotCom">quit tv for our youtube</a></li>
+                <li><a href="http://www.twitch.tv/zomgclan">justin.tv stream to ignore</a></li>
+                <li><a href="http://steamcommunity.com/groups/ZomgClan">our closed steam group</a></li>
+            </ul>
+        </li>
+        <li><a href="forum.php" name="ZOMG Clan Vanilla forums">forum no one updates</a></li>
+    </ul>
+</div>
